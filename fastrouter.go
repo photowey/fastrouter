55603451b4954dea6c40290ddde4a178ff8d12b9
photowey <photowey@gmail.com>
2022-06-25 13:52:02 +0800

    feat: optimize fastrouter

--- fastrouter.go
+++ fastrouter.go
@@ -1,20 +1,36 @@
 package fastrouter
 
+//
+// Fastrouter
+//
+
 import (
+	"github.com/photowey/fastrouter/api/filter"
+	"github.com/photowey/fastrouter/api/interceptor"
 	"github.com/photowey/fastrouter/api/request"
 	"github.com/photowey/fastrouter/api/router"
 )
 
-var _router router.IRouter
-
-func init() {
-	_router = router.NewRouter()
+func Register(method, path string, handler request.Handler) {
+	router.Register(method, path, handler)
 }
 
-func Register(method, path string, handler request.Handler) error {
-	return _router.Register(method, path, handler)
+func UnRegister(method, path string, handler request.Handler) error {
+	return router.UnRegister(method, path, handler)
 }
 
-func Dispatch(hctx request.Context) error {
-	return _router.Dispatch(hctx)
+func RegisterFilter(name string, filterx filter.Filter) {
+	router.RegisterFilter(name, filterx)
+}
+
+func UnRegisterFilter(name string, filterx filter.Filter) error {
+	return router.UnRegisterFilter(name, filterx)
+}
+
+func RegisterInterceptor(name string, interceptorx interceptor.Interceptor) {
+	router.RegisterInterceptor(name, interceptorx)
+}
+
+func UnRegisterInterceptor(name string, interceptorx interceptor.Interceptor) error {
+	return router.UnRegisterInterceptor(name, interceptorx)
 }
